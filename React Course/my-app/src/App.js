31b83e6160d647495a88f8a44b2a467ab409d933
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-14 23:05:11 +0545

    editing the react apps

--- React Course/my-app/src/App.js
+++ React Course/my-app/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+let name= "dipeen kaucha Magar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <nav>
+    <li>HHome </li>
+    <li>About </li>
+    <li>News</li>
+   </nav>
+   
+   <div className="hey">
+    <h1>hello {name}</h1>
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus obcaecati dolore blanditiis eaque labore porro! Ipsam, et, placeat ab molestias officiis ullam, eligendi laborum eveniet saepe reprehenderit quis quas cupiditate.
+    Voluptatum omnis maiores saepe aspernatur error illo praesentium quod architecto assumenda ut ipsum sunt asperiores fugiat autem velit quas animi, laboriosam voluptatem non quibusdam. Modi doloremque ab dolorem maiores eos?
+    Minima modi debitis hic officiis iure distinctio, sed quod, laboriosam odit temporibus quibusdam magni qui itaque expedita ipsam illum eligendi fuga culpa dolorum veritatis? Quae reprehenderit sunt atque laboriosam tempora!
+   </p>
+   </div>
+    </>
   );
 }
 
