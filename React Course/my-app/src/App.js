4d479ca2d67bfbc341da84151a718d286c8baddc
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-14 23:25:55 +0545

    creating the nav bar

--- React Course/my-app/src/App.js
+++ React Course/my-app/src/App.js
@@ -5,20 +5,42 @@ let name= "dipeen kaucha Magar";
 function App() {
   return (
     <>
-   <nav>
-    <li>HHome </li>
-    <li>About </li>
-    <li>News</li>
-   </nav>
-   
-   <div className="hey">
-    <h1>hello {name}</h1>
-    <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus obcaecati dolore blanditiis eaque labore porro! Ipsam, et, placeat ab molestias officiis ullam, eligendi laborum eveniet saepe reprehenderit quis quas cupiditate.
-    Voluptatum omnis maiores saepe aspernatur error illo praesentium quod architecto assumenda ut ipsum sunt asperiores fugiat autem velit quas animi, laboriosam voluptatem non quibusdam. Modi doloremque ab dolorem maiores eos?
-    Minima modi debitis hic officiis iure distinctio, sed quod, laboriosam odit temporibus quibusdam magni qui itaque expedita ipsam illum eligendi fuga culpa dolorum veritatis? Quae reprehenderit sunt atque laboriosam tempora!
-   </p>
-   </div>
+   <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
     </>
   );
 }
