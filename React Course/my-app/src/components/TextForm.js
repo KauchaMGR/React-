d13fbc385348df35c45e3f93dee6b385e2e711da
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-19 08:34:26 +0545

    resizing the textbox and removing the label

--- React Course/my-app/src/components/TextForm.js
+++ React Course/my-app/src/components/TextForm.js
@@ -3,8 +3,8 @@ export default function TextForm(props) {  // Change from textform to TextForm
     <div>
       <div className="mb-3">
         <h1>{props.heading}</h1>
-        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="12"></textarea>
       </div>
     </div>
   )
