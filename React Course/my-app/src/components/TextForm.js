2bcba49db5b99c596104d58b889bf123cca588ad
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-19 08:52:01 +0545

    adding the button

--- React Course/my-app/src/components/TextForm.js
+++ React Course/my-app/src/components/TextForm.js
@@ -5,6 +5,10 @@ export default function TextForm(props) {  // Change from textform to TextForm
         <h1>{props.heading}</h1>
         
         <textarea className="form-control" id="exampleFormControlTextarea1" rows="12"></textarea>
+        <br />
+        <button className="btn btn-primary">
+          convert to upper case
+        </button>
       </div>
     </div>
   )
