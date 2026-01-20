1d1dd6a721483b1d1431e41ed9f9600142dbd11d
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-20 23:36:00 +0545

    fixing the  number of code calculation

--- React Course/my-app/src/components/TextForm.js
+++ React Course/my-app/src/components/TextForm.js
@@ -4,21 +4,21 @@ import React,{useState}from 'react'
 
 export default function TextForm(props) {  // Change from textform to TextFortext
 const handleUpClick =()=>{
-  console.log("hanleupclick was invoked"+text);
+  // console.log("hanleupclick was invoked"+text);
   let newtext=text.toUpperCase();
 
   settext(newtext);
   
 }
 const handlechange =(event)=>{
-  console.log("hanlechange  was invoked");
+  // console.log("hanlechange  was invoked");
   settext(event.target.value);// allows to write more in the text feild
 }
   const[text,settext]=useState("enter the text1")//use of state variable
   //settext("Hey i have ste the text through settext function that i have defined using the state variable")
   return (
     <div>
-      <div className="mb-3">
+      <div className=" container mb-3">
         <h1>{props.heading}</h1>
         
         <textarea className="form-control" value={text} 
@@ -27,6 +27,14 @@ const handlechange =(event)=>{
         <button className="btn btn-primary"  onClick={handleUpClick} >
           convert to upper case
         </button>
+      </div>
+      <div className="container my-3">
+        <h1>
+          The summary of the given text is
+         </h1>
+         <p>
+        total number of words{text.trim().length === 0 ? 0 : text.trim().split(/\s+/).length}and nuber of characters{text.length}.
+         </p>
       </div>
     </div>
   )
