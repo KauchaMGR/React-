96787b21a2659ca4871547f36e4d368777033e6a
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-27 08:33:54 +0545

    fixing the clsss name

--- React Course/my-app/src/components/Alert.js
+++ React Course/my-app/src/components/Alert.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Alert(props) {
   return (
     <div>
-      <div classNameName="alert alert-warning alert-dismissible fade show" role="alert">
-         {props.alert}
+      <div className="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong> {props.alert}</strong>
   <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
     </div>
