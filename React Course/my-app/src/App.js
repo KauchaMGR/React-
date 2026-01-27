9cd00c4b4b18393760bd7f51630e1f2b1d6fa719
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-27 08:28:09 +0545

    ading the alert component

--- React Course/my-app/src/App.js
+++ React Course/my-app/src/App.js
@@ -3,17 +3,32 @@ import './App.css';
 import Navbar from './components/Navbar';
 //import TextForm from './components/TextForm';
 import About from './components/about';
-
-
+import React, { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
+  const [mode, setMode] = useState('light'); // Start with light mode
+  
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#212529';
+      document.body.style.color = 'white';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+    }
+  };
+
   return (
     <>
-      <Navbar title="textutils" home="about home" news="about news" />
-     {/* <div className="container "> <TextForm  heading="Hey enter the text here"/></div> */}
-     <About />
+      <Navbar title="textutils" home="about home" news="about news" mode={mode} toggleMode={toggleMode} />
+       <Alert />
+      {/* <div className="container "> <TextForm  heading="Hey enter the text here"/></div> */}
+      <About />
+     
     </>
-  
   );
 }
 
--- React Course/my-app/src/components/Navbar.js
+++ React Course/my-app/src/components/Navbar.js
@@ -1,42 +1,52 @@
 import React from 'react'
-import PropTypes from 'prop-types'  // Add this import
+import PropTypes from 'prop-types'
 
-export default function Navbar(props
-) {
+export default function Navbar(props) {
   return (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">{props.title}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">{props.home}</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">{props.news}</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">{props.title}</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">{props.home}</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">{props.news}</a>
+            </li>
+          </ul>
+          <div className="form-check form-switch">
+            <input 
+              className="form-check-input" 
+              type="checkbox" 
+              role="switch" 
+              id="flexSwitchCheckDefault"
+              onClick={props.toggleMode}
+            />
+            <label className={`form-check-label text-${props.mode === 'light' ? 'dark' : 'light'}`} htmlFor="flexSwitchCheckDefault">
+              Enable Dark Mode
+            </label>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
-// PropTypes should be defined OUTSIDE the component
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   home: PropTypes.string,
-  news: PropTypes.string
+  news: PropTypes.string,
+  mode: PropTypes.string,
+  toggleMode: PropTypes.func.isRequired
 }
-Navbar.defaultProps={
-    title:"hey its test",
-    home:"i am home",
-    news:"i am news"
+
+Navbar.defaultProps = {
+  title: "hey its test",
+  home: "i am home",
+  news: "i am news",
+  mode: "light"
 }
