e6720a0ea377b442341ea9da66b8f89605473155
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-01-30 09:00:08 +0545

    Making the success buttons to uppercase .

--- React Course/my-app/src/components/Alert.js
+++ React Course/my-app/src/components/Alert.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
+
 function Alert(props) {
+  const setUpper=(str)=>{
+    let st=str.toLowerCase();
+    let ch=st.charAt(0).toUpperCase()+st.slice(1);
+    return ch;
+
+  }
   return (
     props.alert.ty && <div>
     <div className={` alert alert-${props.alert.ty} alert-dismissible fade show `}role="alert">
-        <strong> {props.alert.ty}</strong>:{props.alert.msg}
+        <strong> {setUpper(props.alert.ty)}</strong> : {props.alert.msg}
   <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
     </div>
