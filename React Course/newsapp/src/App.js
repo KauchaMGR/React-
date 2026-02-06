4baed8e0415cd4990c89169f5ccede968284f5ad
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-02-06 21:39:24 +0545

    setting up the Class based component

---
+++ React Course/newsapp/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  name='dipeen';
+  render() {
+    return (
+      <div>
+       <h1> hello my first calss based component {this.name}</h1>
+      </div>
+    )
+  }
+}
