2b70c628019c5df01c8c5c66caf4b1a987be2b5d
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-02-07 21:03:06 +0545

    addingh navb ar in the  news app

--- React Course/newsapp/src/App.js
+++ React Course/newsapp/src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
 
 export default class App extends Component {
   name='dipeen';
   render() {
     return (
-      <div>
+      <div> <Navbar/>
        <h1> hello my first calss based component {this.name}</h1>
+      
       </div>
     )
   }
