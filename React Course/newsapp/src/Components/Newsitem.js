9de24be03a1e2ab21277e8cb0a9f3ea809995a2b
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-03-10 11:02:54 +0545

    handling th e image null urls

--- React Course/newsapp/src/Components/Newsitem.js
+++ React Course/newsapp/src/Components/Newsitem.js
@@ -5,8 +5,13 @@ export class Newsitem extends Component {
     let {title,description,Url,newsUrl}=this.props;
     return (
       <div >
-        <div className="card" style={{width: "18rem;"}}>
-  <img src={Url} className="card-img-top" alt="..."/>
+        <div className="card" style={{width: "18rem"}}>
+<img 
+  src={Url || "https://placehold.co/600x400?text=No+Image"}
+  onError={(e) => { e.target.src = "https://placehold.co/600x400?text=No+Image" }}
+  className="card-img-top" 
+  alt="news thumbnail"
+/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
